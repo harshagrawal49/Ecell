f4d14f2ccc3d48a447dad1c2cddee0de4bf2a660
Harsh Agrawal <agrawalharsh0404@gmail.com>
2025-01-23 09:47:02 +0530

    respoonsiveness

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -46,29 +46,28 @@ const FirstPage = () => {
   return (
     <div className="flex flex-col h-screen w-screen">
       <Navbar />
-      <div className="flex flex-wrap justify-center items-center gap-4 p-4">
-        
-        <div className="order-1 sm:order-3 w-40 h-40 lg:w-60 lg:h-60 bg-gray-300 rounded-lg shadow-inner grid grid-cols-3 grid-rows-3 gap-2 p-5">
-          {gridColors.map((color, index) => (
-            <div
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-4 p-4">
+        <div className="flex md:flex-row gap-5 md:order-2 flex-col">
+          <div className="order-1 w-40 h-40 lg:w-60 lg:h-60 bg-gray-300 rounded-lg shadow-inner grid grid-cols-3 grid-rows-3 gap-2 p-5 ">
+            {gridColors.map((color, index) => (
+              <div
               key={index}
               className={`w-full h-full ${color} rounded-lg`} 
               onClick={() => handleSquareClick(index)}
-            ></div>
-          ))}
-        </div>
+              ></div>
+            ))}
+          </div>
 
-        
-        <div className="order-2 w-40 h-40  sm:w-40 sm:h-40 lg:w-60 lg:h-60 bg-white rounded-lg shadow-lg flex justify-center items-center overflow-hidden border-[1px] border-black">
-          <img
-            src="/Ecell.png"
-            alt="E-Cell Logo"
-            className="w-full h-full object-cover"
-          />
+          <div className="order-2 w-40 h-40  sm:w-40 sm:h-40 lg:w-60 lg:h-60 bg-white rounded-lg shadow-lg flex justify-center items-center overflow-hidden border-[1px] border-black">
+            <img
+              src="/Ecell.png"
+              alt="E-Cell Logo"
+              className="w-full h-full object-cover"
+              />
+          </div>
         </div>
-
         
-        <div className="order-3 sm:order-1 w-40 h-40 sm:w-40 sm:h-40 lg:w-60 lg:h-60 bg-gradient-to-br from-blue-700 to-magenta rounded-lg shadow-lg"></div>
+        <div className=" md:order-1 sm:order-1 md:w-[350px] w-40 h-40 sm:w-40 sm:h-40 lg:w-60 lg:h-60 bg-gradient-to-br from-blue-700 to-magenta rounded-lg shadow-lg"></div>
       </div>
     </div>
   );
